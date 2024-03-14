535ec1a69b6ac24d6faddaf7af6c1132b609cbc4
sandrabusik-pvcc <160065205+sandrabusik-pvcc@users.noreply.github.com>
2024-03-13 21:47:07 -0400

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,26 @@
+# Name:Sandra Busik
+# Program Purpose: Hello World in Python
+
+
+h
+
+print('I love Python!')
+print("\n\t\\Hello\\ \n\t\tWorld!")
+
+#Pyton can do arithmetic too.
+aiden = 20
+albert = 19
+drstevens = 68
+sum_ages = aiden + albert + drstevens
+ave = sum_ages / 3
+print("The average age is:", ave)
+#Next year:
+aiden += 1
+albert += 1
+drstevens += 1
+sum_ages = aiden + albert + drstevens
+ave = sum_ages / 3
+print("Aiden next year:", aiden)
+print("Next year, the average age will be:", ave)
+
+
